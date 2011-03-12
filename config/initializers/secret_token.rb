c5521dd893668c86c58c4211f2657aa099be70ad
Alex.JeSa <sanek.app@gmail.com>
2011-03-13 01:03:24 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Southwushu::Application.config.secret_token = '3f0d053db5e863ffc2e82192b8e51d062160eea8460c12f7044af9b30ed0bc4b95d2602fb721e263b34e8bb4aeb5d9d51d2e4bfe0c20b054f71a08ade4b38b8b'
